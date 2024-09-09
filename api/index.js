4d648868cc31ac218e0934bc84bb85490e72b53f
khanef <vlpkhang.20it9@vku.udn.vn>
2024-09-09 23:06:19 +0700

    add redux persist

--- api/index.js
+++ api/index.js
@@ -13,8 +13,8 @@ route(app)
 
 db.connect()
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000!')
+app.listen(process.env.PORT, () => {
+    console.log('Server is running !')
 })
 
 app.use((err, req, res, next) => {

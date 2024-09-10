f8ef74d3f92b59e0a1d69ce318460e323d1ba717
khanef <vlpkhang.20it9@vku.udn.vn>
2024-09-11 03:48:32 +0700

    update and fix header component with user data

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -73,7 +73,7 @@ export const google = async (req, res, next) => {
                 profilePicture: googlePhotoUrl,
             })
             await newUser.save()
-            const token = jwt.sign({id: newUseruser._id}, process.env.JWT_SECRET)
+            const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET)
             const {password, ...rest} = newUser._doc
             res.status(200).cookie('access_token', token, {
                 httpOnly: true
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,9 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import useReducer from './user/userSlice'
-import {persistReducer, persistStore} from 'redux-persist'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import userReducer from './user/userSlice'
+import { persistReducer, persistStore } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
-const rootReducer = combineReducers({
-  user: useReducer
-})
+const rootReducer = combineReducers({user: userReducer})
 
 const persistConfig = {
   key: 'root',
@@ -17,7 +15,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false}),
+  middleware : (getDefaultMiddleware) => getDefaultMiddleware ({
+    serializableCheck: false
+  })
 })
 
 export const persistor = persistStore(store)

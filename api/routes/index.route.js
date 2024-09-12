6d7ab2118b864201c378187a9149551db7467eb8
khanef <vlpkhang.20it9@vku.udn.vn>
2024-09-13 02:09:17 +0700

    add create a post api route

--- api/routes/index.route.js
+++ api/routes/index.route.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import authRouter from './auth.route.js'
 import userRouter from './user.route.js'
+import postRouter from './post.route.js'
 
 const app = express();
 
 function route(app){
     app.use('/api/auth', authRouter)
     app.use('/api/user', userRouter)
+    app.use('/api/post', postRouter)
 }
 
 export default route

3728338733f504aa12356771c205d12f33f1ed98
khanef <vlpkhang.20it9@vku.udn.vn>
2024-09-22 22:14:43 +0700

    add like functionality to the comment component

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { createcomment, getpostcomment } from '../controllers/comment.controller.js'
+import { createcomment, getpostcomment, likeComment } from '../controllers/comment.controller.js'
 const router = express.Router()
 
 router.post('/create',verifyToken, createcomment)
 router.get('/getpostcomment/:postId', getpostcomment)
+router.put('/likecomment/:commentId', verifyToken, likeComment)
 
 export default router
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react"
 import moment from 'moment'
+import {FaThumbsUp} from 'react-icons/fa'
+import { useSelector } from "react-redux"
 
-export default function Comment({comment}) {
+export default function Comment({comment, onLike}) {
     const [user, setUser] = useState({})
-    console.log(user);
+    const {currentUser} = useSelector((state) => state.user)
     
     useEffect(() => {
         const getUser = async () => {
             try {
                 const res = await fetch(`/api/user/${comment.userId}`)
                 const data = await res.json()
-                console.log(data)
                 if(res.ok){
                     setUser(data)
                 }
@@ -31,6 +32,20 @@ export default function Comment({comment}) {
                 <span className="text-xs text-gray-500">{moment(comment.createdAt).fromNow()}</span>
             </div>
             <p className="text-gray-500 pb-2">{comment.content}</p>
+            <div className="flex items-center pt-2 text-xs border-t dark:text-gray-700 max-w-fit gap-2">
+                <button 
+                    type="button" 
+                    onClick={() => onLike(comment._id)} 
+                    className={`text-gray-400 hover:text-blue-500 ${ currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}
+                >
+                    <FaThumbsUp className="text-sm"/>
+                </button>
+                <p className="text-gray-400">
+                    {
+                        comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")
+                    }
+                </p>
+            </div>
         </div>
     </div>
   )

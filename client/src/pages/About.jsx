c2e07d32bf59983b1a816adad0ddabe7ce7b6f08
khanef <vlpkhang.20it9@vku.udn.vn>
2024-10-05 17:25:58 +0700

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-2 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>About Khan's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+            Bạn có phải là người đam mê cây xanh, yêu thích việc chăm sóc và ngắm nhìn những chậu cây xanh mướt? Hay đơn giản bạn chỉ muốn tìm kiếm một không gian xanh mát cho căn nhà của mình? Khan's Blog là nơi lý tưởng dành cho bạn, một thế giới xanh tươi đầy ắp kiến thức và trải nghiệm về cây cảnh.
+            </p>
+
+            <p>
+            Tại Khan's Blog, bạn sẽ được khám phá vô số chủ đề thú vị về thế giới cây xanh, từ những kiến thức cơ bản về cách trồng, chăm sóc, nhân giống cây đến những kỹ thuật nâng cao, những bí mật để tạo ra một khu vườn đẹp mắt và đầy sức sống. Khan, với tâm huyết và kinh nghiệm dày dặn của mình, sẽ dẫn dắt bạn đi qua từng giai đoạn, chia sẻ những bí quyết và kinh nghiệm thực tế để bạn tự tin chăm sóc cây cảnh của mình.
+            </p>
+
+            <p>
+              Bạn sẽ tìm thấy những bài viết chi tiết về từng loại cây, từ những giống cây phổ biến như hoa hồng, lan, sen đá cho đến những loại cây độc đáo, ít người biết đến. Bên cạnh đó, Khan's Blog còn cung cấp những mẹo vặt hữu ích, những lời khuyên thiết thực giúp bạn giải quyết những vấn đề thường gặp khi chăm sóc cây cảnh, giúp bạn tự tin hơn trong việc vun trồng và tạo ra một không gian xanh mát, tràn đầy sức sống cho chính ngôi nhà của mình. Hãy cùng Khan khám phá thế giới cây cảnh và biến ước mơ về một khu vườn xanh mát của bạn thành hiện thực! 
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }

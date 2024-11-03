ff5d4a9d9e5694162c74c6c4d5d7e2822d6f72ca
khanef <vlpkhang.20it9@vku.udn.vn>
2024-11-03 19:06:32 +0700

    update 03/11/2024

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -69,7 +69,7 @@ export default function PostPage() {
             <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
             <span className='italic'>{post && (post.content.length / 1000).toFixed(0)} mins read</span>
         </div>
-        <div className='p-3 max-w-4xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
+        <div className='p-3 max-w-4xl mx-auto w-full post-content text-justify' dangerouslySetInnerHTML={{__html: post && post.content}}>
 
         </div>
         <div className='max-w-4xl mx-auto w-full'>
